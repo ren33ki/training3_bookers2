9e1361d930ec5be7a8a033dae68679c3edb552d2
efficient-life <efficient122@gmail.com>
2022-05-25 12:20:58 +0900

    origin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get "home/about"=>"homes#about"
+  devise_for :users
+  resources :users
+  resources :books 
+  root :to =>"homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    name: "aa",
+    email: "a@a",
+    password: "aaaaaa"
+)
+
+5.times do |n|
+    User.create!(
+        name: Faker::Name.first_name,
+        email: Faker::Internet.email,
+        introduction: Faker::Lorem.characters(number: 20),
+        password: Faker::Lorem.characters(number: 10)
+    )
+    
+    Book.create!(
+        title: Faker::Book.title,
+        body: "ランダム#{Faker::Lorem.characters(number: 5)}",
+        user_id: 1
+        )
+end
+
+3.times do |n|
+    Book.create!(
+        title: Faker::Book.title,
+        body: "ランダム#{Faker::Lorem.characters(number: 5)}",
+        user_id: 3
+        )
+end
+
