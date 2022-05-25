d19c6a752188d19711b8a1d9f8914aa2ad4d1b6f
efficient-life <efficient122@gmail.com>
2022-05-25 12:40:13 +0900

    [Add] bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "home/about"=>"homes#about"
-  devise_for :users
   resources :users
   resources :books 
   root :to =>"homes#top"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
